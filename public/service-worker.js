d01c81f3bb6c77596ab214f4205ce531f9063844
lindsfitz <lindsaytfitz@gmail.com>
2021-12-05 13:44:15 -0800

    attempt to fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
-    '/style.css',
+    '/styles.css',
     '/db.js',
     "/index.js",
     "/icons/icon-512x512.png",
@@ -10,55 +10,69 @@ const FILES_TO_CACHE = [
     'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
   ];
   
-  const PRECACHE = 'precache-v1';
-  const RUNTIME = 'runtime';
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
   
   self.addEventListener('install', (event) => {
     event.waitUntil(
       caches
-        .open(PRECACHE)
-        .then((cache) => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
+        .open(DATA_CACHE_NAME)
+        .then((cache) => cache.add('/api/transaction'))
     );
+
+    event.waitUntil(caches.open(CACHE_NAME)
+    .then((cache) => cache.addAll(FILES_TO_CACHE)))
+
+    self.skipWaiting()
   });
   
   // The activate handler takes care of cleaning up old caches.
   self.addEventListener('activate', (event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
     event.waitUntil(
-      caches
-        .keys()
-        .then((cacheNames) => {
-          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-        })
-        .then((cachesToDelete) => {
-          return Promise.all(
-            cachesToDelete.map((cacheToDelete) => {
-              return caches.delete(cacheToDelete);
-            })
-          );
-        })
-        .then(() => self.clients.claim())
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
     );
+  
+    self.clients.claim();
   });
   
   self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith(self.location.origin)) {
+    if (event.request.url.includes("/api/")) {
       event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-          if (cachedResponse) {
-            return cachedResponse;
-          }
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
   
-          return caches.open(RUNTIME).then((cache) => {
-            return fetch(event.request).then((response) => {
-              return cache.put(event.request, response.clone()).then(() => {
-                return response;
-              });
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(event.request);
             });
-          });
-        })
+        }).catch(err => console.log(err))
       );
+  
+      return;
     }
+  
+    event.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.match(event.request).then(response => {
+          return response || fetch(event.request);
+        });
+      })
+    );
   });
   
