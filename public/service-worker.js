549d72858f7a98df238e56ae07d890d8c770f640
lindsfitz <lindsaytfitz@gmail.com>
2021-12-06 14:10:30 -0800

    heroku fixes

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,8 +8,6 @@ const FILES_TO_CACHE = [
     "/index.js",
     "/icons/icon-512x512.png",
     "/icons/icon-192x192.png",
-    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-    'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
   ];
 
   const PRECACHE = 'precache-v1';
